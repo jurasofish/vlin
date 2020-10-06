0f070299d5fba5674e22555fc3352b09190502f6
Michael Jurasovic <mjurasovic@gmail.com>
2020-10-06 22:25:30 +1100

    Update __init__.py

--- vlin/__init__.py
+++ vlin/__init__.py
@@ -1,29 +1,96 @@
 import numpy as np
-import scipy.sparse as spa
+import scipy.sparse as sparse
+from typing import List, Union
+from numbers import Real
 
 
-class Expr(spa.csr_matrix):
+class Expr(sparse.csr_matrix):
+    """ A vector of linear expressions: Each row is a vector of var coefficients. """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def raw(self) -> sparse.csr_matrix:
+        """ Convert expression to raw sparse matrix. """
+        return sparse.csr_matrix(self)
 
-class Cons(spa.csr_matrix):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def promote_constant(self, k: Real) -> 'Expr':
+        """ Convert a Real number into a constant-valued linear expression. """
+        expr = sparse.csr_matrix(self.shape, dtype=self.dtype)
+        expr[:, 0] = 1
+        expr *= k
+        return self.__class__(expr)
+
+    def __le__(self, other: Union['Expr', Real]) -> List['Expr']:
+        """ x <= y  =>  x-y <= 0 """
+        return [self - other]
+
+    def __ge__(self, other: Union['Expr', Real]) -> List['Expr']:
+        """ x >= y  =>  x-y >= 0  =>  y-x <= 0 """
+        return [other - self]
+
+    def __eq__(self, other: Union['Expr', Real]) -> List['Expr']:
+        """ x == y  =>  x-y >= 0 AND x-y <= 0 """
+        return (other <= self) + (self <= other)  # List of two linear expressions.
+
+    def __add__(self, other: Union['Expr', Real]) -> 'Expr':
+        if isinstance(other, Real):
+            other = self.promote_constant(other)
+        return super().__add__(other)
+
+    def __mul__(self, other: Real) -> 'Expr':
+        if not isinstance(other, Real):
+            raise NotImplementedError
+        return super().__mul__(other)
+
+    def __sub__(self, other: Union['Expr', Real]) -> 'Expr':
+        return self.__add__(-1.0 * other)
+
+    def __truediv__(self, other: Real) -> 'Expr':
+        return self.__mul__(1.0/other)
+
+    def __radd__(self, other: Union['Expr', Real]) -> 'Expr':
+        return self.__add__(other)
+
+    def __rsub__(self, other: Union['Expr', Real]) -> 'Expr':
+        return (-self).__add__(other)
+
+    def __rmul__(self, other: Real) -> 'Expr':
+        return self.__mul__(other)
+
+    def __iadd__(self, other):
+        raise NotImplementedError
+
+    def __isub__(self, other):
+        raise NotImplementedError
+
+    def __imul__(self, other):
+        raise NotImplementedError
+
+    def __itruediv__(self, other):
+        raise NotImplementedError
+
+    def __lt__(self, other):
+        raise NotImplementedError
+
+    def __gt__(self, other):
+        raise NotImplementedError
+
+    def __ne__(self, other):
+        raise NotImplementedError
+
+    # TODO: yada yada fill out the rest of these...
 
 
 class Model:
 
-    def __init__(self, max_vars: int = 5, max_constrs: int = 10):
+    def __init__(self, max_vars: int = 5):
         self.max_vars: int = max_vars + 1  # One extra for the constant.
         self.next_var_idx: int = 0  # What number variable model is up to.
-        self.next_con_idx: int = 0  # What number constraint model is up to.
         self.dtype = np.float64
         self.int_vars = np.zeros(max_vars, dtype=np.bool)  # True where var is int
 
         # Constraints are represented as linear expressions which must be <= 0
-        # self.cons = np.zeros((max_constrs, max_vars), dtype=self.dtype)
-        self.cons = Cons((max_constrs, self.max_vars), dtype=self.dtype)
+        self.cons: List[Expr] = []
 
         self.k = self.var()  # A linear expression equal to 1.0
 
@@ -38,33 +105,27 @@ class Model:
         self.int_vars[var_idxs] = integer
         return exprs
 
-    def le(self, x: Expr, y: Expr):
-        """ x <= y  =>  x-y <= 0 """
-        n = x.shape[0]
-        start_idx = self.next_con_idx
-        self.next_con_idx += n
-        self.cons[start_idx + np.arange(n)] = x - y
+    def add_constr(self, constr: List[Expr]):
+        self.cons += constr
 
-    def ge(self, x: Expr, y: Expr):
-        """ x >= y  =>  x-y >= 0  =>  y-x <= 0 """
-        n = x.shape[0]
-        start_idx = self.next_con_idx
-        self.next_con_idx += n
-        self.cons[start_idx + np.arange(n)] = y - x
+    def __iadd__(self, other: List[Expr]):
+        self.add_constr(other)
+        return self
 
-    def eq(self, x: Expr, y: Expr):
-        """ x == y  =>  x-y >= 0 AND x-y <= 0 """
-        self.le(x, y)
-        self.ge(x, y)
+    def combine_cons(self) -> Expr:
+        return sparse.vstack(self.cons, format='csr')
 
 
 def main():
     m = Model()
     a = m.var(2)
     b = m.var(2)
-    m.le(a, b)
-    m.ge(a, b)
-    m.eq(a, b)
+    c = a + 2
+    m += a <= b
+    m += a >= b
+    m += a == b
+    m += c == a
+    print(m.combine_cons().todense())
 
 
 if __name__ == '__main__':
