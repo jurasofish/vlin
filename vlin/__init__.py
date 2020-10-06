5696dc744e571a1f20312b7d78e06a9c80ecb485
Michael Jurasovic <mjurasovic@gmail.com>
2020-10-06 17:20:20 +1100

    fully working with sparse matrices

--- vlin/__init__.py
+++ vlin/__init__.py
@@ -1,9 +1,20 @@
 import numpy as np
+import scipy.sparse as spa
+
+
+class Expr(spa.csr_matrix):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class Cons(spa.csr_matrix):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Model:
 
-    def __init__(self, max_vars: int = 5, max_constrs: int = 5):
+    def __init__(self, max_vars: int = 5, max_constrs: int = 10):
         self.max_vars: int = max_vars + 1  # One extra for the constant.
         self.next_var_idx: int = 0  # What number variable model is up to.
         self.next_con_idx: int = 0  # What number constraint model is up to.
@@ -11,35 +22,37 @@ class Model:
         self.int_vars = np.zeros(max_vars, dtype=np.bool)  # True where var is int
 
         # Constraints are represented as linear expressions which must be <= 0
-        self.cons = np.zeros((max_constrs, max_vars), dtype=self.dtype)
+        # self.cons = np.zeros((max_constrs, max_vars), dtype=self.dtype)
+        self.cons = Cons((max_constrs, self.max_vars), dtype=self.dtype)
 
         self.k = self.var()  # A linear expression equal to 1.0
 
-    def var(self, n=1, integer: bool = False) -> np.ndarray:
+    def var(self, n=1, integer: bool = False) -> Expr:
         """ Return linear expressions representing new variables. """
         start_idx = self.next_var_idx
         self.next_var_idx += n
-        exprs = np.zeros((n, self.max_vars), dtype=self.dtype)
+        # exprs = np.zeros((n, self.max_vars), dtype=self.dtype)
+        exprs = Expr((n, self.max_vars), dtype=self.dtype)
         var_idxs = np.arange(n)
         exprs[var_idxs, start_idx + var_idxs] = 1.0
         self.int_vars[var_idxs] = integer
         return exprs
 
-    def le(self, x: np.ndarray, y: np.ndarray):
+    def le(self, x: Expr, y: Expr):
         """ x <= y  =>  x-y <= 0 """
         n = x.shape[0]
         start_idx = self.next_con_idx
         self.next_con_idx += n
         self.cons[start_idx + np.arange(n)] = x - y
 
-    def ge(self, x: np.ndarray, y: np.ndarray):
+    def ge(self, x: Expr, y: Expr):
         """ x >= y  =>  x-y >= 0  =>  y-x <= 0 """
         n = x.shape[0]
         start_idx = self.next_con_idx
         self.next_con_idx += n
         self.cons[start_idx + np.arange(n)] = y - x
 
-    def eq(self, x: np.ndarray, y: np.ndarray):
+    def eq(self, x: Expr, y: Expr):
         """ x == y  =>  x-y >= 0 AND x-y <= 0 """
         self.le(x, y)
         self.ge(x, y)
