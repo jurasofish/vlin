bf09891bf8584903a0b31e97ac1c458362638ea0
Michael Jurasovic <mjurasovic@gmail.com>
2020-10-08 18:49:43 +1100

    playing

--- vlin/__init__.py
+++ vlin/__init__.py
@@ -6,19 +6,20 @@ from .expressions import Expr, ExprNumpy
 
 class Model:
 
-    def __init__(self, max_vars: int = 5,
+    def __init__(self,
+                 max_vars: int = 5,
                  expr: Expr = ExprNumpy,
                  dtype: np.dtype = np.float):
         self.max_vars: int = max_vars + 1  # One extra for the constant.
         self.next_var_idx: int = 0  # What number variable model is up to.
-        self.dtype = dtype
-        self.expr = expr
+        self.dtype: np.dtype = dtype
+        self.expr: Expr = expr
         self.int_vars = np.zeros(max_vars, dtype=np.bool)  # True where var is int
 
         # Constraints are represented as linear expressions which must be <= 0
         self.cons: List[Expr] = []
 
-        self.k = self.var()  # A linear expression equal to 1.0
+        self.k: Expr = self.var()  # A linear expression equal to 1.0
 
         self.objective: Optional[Expr] = None
 
@@ -32,10 +33,10 @@ class Model:
         self.int_vars[var_idxs] = integer
         return exprs
 
-    def add_constr(self, constr: 'Expr'):
+    def add_constr(self, constr: Expr):
         self.cons.append(constr)
 
-    def __iadd__(self, other: 'Expr'):
+    def __iadd__(self, other: Expr):
         self.add_constr(other)
         return self
 
