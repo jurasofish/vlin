44c895e7c4eaecb0ae481778f860f1521f9e5c3d
Michael Jurasovic <mjurasovic@gmail.com>
2020-10-09 21:38:31 +1100

    black

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 
 setuptools.setup(
-    name='vlin',
-    version='0.0.0',
+    name="vlin",
+    version="0.0.0",
     packages=setuptools.find_packages(),
-    install_requires=['cylp', 'numpy'],
-    python_requires='>=3.7',
+    install_requires=["cylp", "numpy"],
+    python_requires=">=3.7",
     include_package_data=True,
     setup_requires=[],
 )
