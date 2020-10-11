5aee648f3a269ccf92cc4b344ed01e45880e9cf8
Michael Jurasovic <mjurasovic@gmail.com>
2020-10-11 12:55:24 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.0.1",
     packages=setuptools.find_packages(),
     install_requires=["numpy", "scipy"],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     include_package_data=True,
     setup_requires=[],
 )
